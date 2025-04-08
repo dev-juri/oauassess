56cee7af16980b8654cae4c21a7f1dc324d716e1
Oluwafemi <ojurifemi132@gmal.com>
2025-04-08 18:47:42 +0100

    chore: setup dependencies.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const ENV = process.env.NODE_ENV;
+console.log(ENV);
 
 @Module({
-  imports: [],
+  imports: [
+    AdminModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      // load: [appConfig, databaseConfig],
+      // validationSchema: environmentValidation,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('MONGO_URI')
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
